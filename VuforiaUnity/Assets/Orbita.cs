cc540ad08bc13d2394a2904ae522ffc8436f0ee8
delgado.trueba <delgado.trueba@outlook.es>
2019-05-01 15:35:16 +0200

    primer atomo

---
+++ VuforiaUnity/Assets/Orbita.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbita : MonoBehaviour {
+
+	float timeCounter;
+
+	public float speed;
+
+	public float posX;
+	public float posZ;
+
+	// Use this for initialization
+	void Start () {
+		timeCounter = 0f;
+
+		/*speed = 1f;*/
+	
+		if (posX == 0.02f) {
+			timeCounter = 1f;
+		}
+		if (posX == 0.04) {
+			timeCounter = 1.5f;
+		}
+		if (posX == 0.06f) {
+			timeCounter = 1.75f;
+		}	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeCounter += Time.deltaTime * speed;
+
+		float x = Mathf.Cos (timeCounter) * posX;
+		float y = 0.00f;
+		float z = Mathf.Sin (timeCounter) * posZ;
+
+		transform.position = new Vector3 (x, y, z);
+	}
+}
