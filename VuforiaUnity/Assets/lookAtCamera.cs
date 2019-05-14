97e339dead0c9f13d995992f790918acc139a059
delgado.trueba <delgado.trueba@outlook.es>
2019-05-14 12:27:30 +0200

    mostrador de información en hidrogeno

---
+++ VuforiaUnity/Assets/lookAtCamera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lookAtCamera : MonoBehaviour {
+
+	private GameObject cam;
+
+	// Use this for initialization
+	void Start () {
+		cam =  GameObject.Find("ARCamera");;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 mirarHaciaCamaraSinCambiarEjeX 
+			= new Vector3 (this.transform.position.x, cam.transform.position.y, cam.transform.position.z);
+
+		this.transform.LookAt(mirarHaciaCamaraSinCambiarEjeX);
+	}
+}
