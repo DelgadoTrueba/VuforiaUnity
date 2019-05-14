37af97fe87d0cfd6a3e9e0b186b01fe0d8e8360b
lkristianl <kristi.magjistari@gmail.com>
2019-05-14 14:07:58 +0200

    mostrarDatos atomo on I

---
+++ VuforiaUnity/Assets/mostratDatos.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mostratDatos : MonoBehaviour {
+
+
+    public GameObject texto;
+
+	// Use this for initialization
+	void Start () {
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            texto.SetActive(true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.I))
+        {
+            texto.SetActive(false);
+        }
+    }
+}
