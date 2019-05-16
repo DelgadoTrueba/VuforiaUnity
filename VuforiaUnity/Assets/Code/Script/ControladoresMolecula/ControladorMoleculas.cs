63cd5ca832fbdbb899df29ca2d982a4c92374305
delgado.trueba <delgado.trueba@outlook.es>
2019-05-16 22:57:53 +0200

    molecula de monoxido de cloro y sal comun ok

--- VuforiaUnity/Assets/Code/Script/ControladoresMolecula/ControladorMoleculas.cs
+++ VuforiaUnity/Assets/Code/Script/ControladoresMolecula/ControladorMoleculas.cs
@@ -9,6 +9,9 @@ public class ControladorMoleculas : MonoBehaviour {
 	public GameObject targetHidrogeno;
 	public GameObject targetCarbono;
 	public GameObject targetNitrogeno;
+	public GameObject targetSodio;
+	public GameObject targetCloro;
+
 
 	//Molecula de Agua
 	public bool isOxigeno;
@@ -25,12 +28,23 @@ public class ControladorMoleculas : MonoBehaviour {
 	//public bool isHidrogeno;
 	public GameObject moleculaDeAmoniaco;
 
+	//Molecula de Monoxido De Carbono
+	//public bool isOxigeno;
+	//public bool isCarbono;
+	public GameObject moleculaDeMonoxidoDeCarbono;
+
+	//Molecula Sal Comun
+	public bool isSodio;
+	public bool isCloro;
+	public GameObject moleculaDeSalComun;
 
 	// Atomos
 	private GameObject atomoOxigeno;
 	private GameObject atomoHidrogeno;
 	private GameObject atomoCarbono;
 	private GameObject atomoNitrogeno;
+	private GameObject atomoSodio;
+	private GameObject atomoCloro;
 
 
 	// Use this for initialization
@@ -39,12 +53,15 @@ public class ControladorMoleculas : MonoBehaviour {
 		isHidrogeno = false;
 		isCarbono = false;
 		isNitrogeno = false;
-
+		isSodio = false;
+		isCloro = false;
 
 		atomoOxigeno = targetOxigeno.transform.GetChild (0).gameObject;
 		atomoHidrogeno = targetHidrogeno.transform.GetChild (0).gameObject;
 		atomoCarbono = targetCarbono.transform.GetChild (0).gameObject;
-		atomoNitrogeno = targetNitrogeno.transform.GetChild (0).gameObject;	
+		atomoNitrogeno = targetNitrogeno.transform.GetChild (0).gameObject;
+		atomoSodio = targetSodio.transform.GetChild (0).gameObject;
+		atomoCloro = targetCloro.transform.GetChild (0).gameObject;
 	}
 
 	// Update is called once per frame
@@ -95,17 +112,51 @@ public class ControladorMoleculas : MonoBehaviour {
 				moleculaDeAmoniaco.SetActive (true);
 			} 
 		}
+		else if(condicionesNecesariasMoledulaDeMonoxidoDeCarbono())
+		{
+			Vector3 posCarbono = targetCarbono.transform.position;
+			Vector3 posOxigeno = targetOxigeno.transform.position;
+
+			if (distanciaMinimaParaGenerarMoledula(posCarbono, posOxigeno))
+			{
+				moleculaDeMonoxidoDeCarbono.transform.position = 
+					calcularPtoMedio (posCarbono, posOxigeno);
+
+				atomoCarbono.SetActive (false);
+				atomoOxigeno.SetActive (false);
+				moleculaDeMonoxidoDeCarbono.SetActive (true);
+			} 
+		}
+		else if(condicionesNecesariasMoledulaDeSalComun())
+		{
+			Vector3 posSodio = targetSodio.transform.position;
+			Vector3 posCloro = targetCloro.transform.position;
+
+			if (distanciaMinimaParaGenerarMoledula(posSodio, posCloro))
+			{
+				moleculaDeSalComun.transform.position = 
+					calcularPtoMedio (posSodio, posCloro);
+
+				atomoSodio.SetActive (false);
+				atomoCloro.SetActive (false);
+				moleculaDeSalComun.SetActive (true);
+			} 
+		}
 		else 
 		{
 			moleculaDeAgua.SetActive(false);
 			moleculaDeMetano.SetActive (false);
 			moleculaDeAmoniaco.SetActive (false);
+			moleculaDeMonoxidoDeCarbono.SetActive (false);
+			moleculaDeSalComun.SetActive (false);
 
 
 			atomoOxigeno.SetActive(true);
 			atomoHidrogeno.SetActive(true);
 			atomoCarbono.SetActive (true);
 			atomoNitrogeno.SetActive (true);
+			atomoSodio.SetActive (true);
+			atomoCloro.SetActive (true);
 		}
 
 
@@ -121,6 +172,14 @@ public class ControladorMoleculas : MonoBehaviour {
 
 	private bool condicionesNecesariasMoledulaDeAmoniaco(){
 		return this.isNitrogeno && this.isHidrogeno;
+	}
+
+	private bool condicionesNecesariasMoledulaDeMonoxidoDeCarbono(){
+		return this.isCarbono && this.isOxigeno;
+	}
+
+	private bool condicionesNecesariasMoledulaDeSalComun(){
+		return this.isSodio && this.isCloro;
 	}
 
 	private Vector3 calcularPtoMedio(Vector3 posTarget1, Vector3 posTarget2){
---
+++ VuforiaUnity/Assets/Code/Script/TrackersAtomo/TrackerSodio.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*add vuforia*/
+using Vuforia;
+
+public class TrackerSodio : MonoBehaviour, ITrackableEventHandler {
+
+	private TrackableBehaviour mTrackableBehaviour;
+
+	public GameObject controladorMoleculas;
+
+	void Start () {
+
+		mTrackableBehaviour = GetComponent<TrackableBehaviour> ();
+
+		if (mTrackableBehaviour) {
+			mTrackableBehaviour.RegisterTrackableEventHandler (this);
+		}		
+	}
+
+
+	void Update () {
+
+	}
+
+	public void OnTrackableStateChanged(
+		TrackableBehaviour.Status previousStatus,
+		TrackableBehaviour.Status newStatus)
+	{
+		if (newStatus == TrackableBehaviour.Status.DETECTED ||
+			newStatus == TrackableBehaviour.Status.TRACKED ||
+			newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+		{
+			OnTrackingFound();
+		}
+		else
+		{
+			OnTrackingLost();
+		}
+	}
+
+	private void OnTrackingFound()
+	{
+		Debug.Log ("Hidrogeno: Hola");
+		controladorMoleculas.GetComponent<ControladorMoleculas> ().isSodio = true;
+	}
+	private void OnTrackingLost()
+	{
+		Debug.Log ("Hidrogeno: Adios");
+		controladorMoleculas.GetComponent<ControladorMoleculas> ().isSodio = false;
+	}
+}
